c61cf1ac17a2a3a18919e7aa250b3fda4776af7d
Claudio Moreno <clauartur98@gmail.com>
2020-05-25 01:07:17 -0500

    feat: add question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,12 +27,20 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
+
+  /* List<String> questions = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.'
   ];
-  List<bool> answers = [false, true, true];
+  List<bool> answers = [false, true, true]; */
+
+  List<Question> questionbank = [
+    Question(q:'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q:'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q:'A slug\'s blood is green.', a: true),
+  ];
+
   int questionNumber = 0;
 
   @override
@@ -46,7 +55,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+               questionbank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,7 +80,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = questionbank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                 } else {}
                 setState(() {
@@ -97,7 +106,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
                 setState(() {
                   //The user picked true.
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer = questionbank[questionNumber].questionAnswer;
                   if (correctAnswer == false) {
                   } else {}
                   questionNumber++;
